eec58afa124b285a562c71ef747c5c183322867a
tom <kastytom@gmail.com>
2022-07-11 19:18:28 +0300

    1st commit

--- src/sections/team-section.js
+++ src/sections/team-section.js
@@ -108,9 +108,9 @@ const styles = {
     mt: [0, null, -6, null, -4],
     gridGap: ['35px 0px', null, 0, null, null, '30px 35px'],
     gridTemplateColumns: [
-      'repeat(2,1fr)',
+      'repeat(1,1fr)',
       null,
-      'repeat(2,1fr)',
+      'repeat(1,1fr)',
       null,
       'repeat(2,1fr)',
     ],
