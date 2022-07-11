0773ca43e196909e07f6769299fb17607588cb08
tom <kastytom@gmail.com>
2022-07-11 20:13:45 +0300

    1st commit

--- src/sections/feature.js
+++ src/sections/feature.js
@@ -4,9 +4,9 @@ import { Container, Grid } from 'theme-ui';
 import SectionHeader from 'components/section-header';
 import FeatureCard from 'components/feature-card.js';
 import Performance from 'assets/feature/performance.png';
-import Partnership from 'assets/feature/partnership.svg';
-import Subscription from 'assets/feature/subscription.svg';
-import Support from 'assets/feature/support.svg';
+import Partnership from 'assets/feature/partnership.png';
+import Subscription from 'assets/feature/subscription.png';
+import Support from 'assets/feature/support.png';
 
 const data = [
   {
